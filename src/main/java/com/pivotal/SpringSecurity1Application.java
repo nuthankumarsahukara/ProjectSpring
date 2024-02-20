e2f24fcf4ac1fa9d10953843d03fb4e303abf52c
nuthankumarsahukara <142878837+nuthankumarsahukara@users.noreply.github.com>
2024-02-20 09:21:58 +0530

    Update SpringSecurity1Application.java

--- src/main/java/com/pivotal/SpringSecurity1Application.java
+++ src/main/java/com/pivotal/SpringSecurity1Application.java
@@ -2,13 +2,19 @@ package com.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringSecurity1Application {
+public class SpringSecurity1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurity1Application.class, args);
 		System.out.println("application started");
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringSecurity1Application.class);
+		
 	}
 
 }
